b0865f545bb0ecd9b0b740278653d19b01560bcd
Youngmin Kim <ymkim92@gmail.com>
2024-08-08 19:21:57 +1000

    WIP ..

--- fake_sensor/protocol/field_status.py
+++ fake_sensor/protocol/field_status.py
@@ -5,15 +5,16 @@ class Status:
 
     # TO get the number of status, it should start with "STATUS_"
     # DO NOT create an empty bit. Use reserved bit
-    STATUS_DATA1_READY = 1 << 0
-    STATUS_DATA2_READY = 1 << 1
-    STATUS_RESERVED1 = 1 << 2
-    STATUS_OVERRUN_ERROR = 1 << 3
+    STATUS_DATA1_READY = 0
+    STATUS_DATA2_READY = 1
+    STATUS_RESERVED1 = 2
+    STATUS_OVERRUN_ERROR = 3
 
-    _constant_map = {
+    status_name_map = {
         STATUS_DATA1_READY: "DATA1_READY",
         STATUS_DATA2_READY: "DATA2_READY",
         STATUS_RESERVED1: "RESERVED1",
+        # This must be the last one
         STATUS_OVERRUN_ERROR: "OVERRUN_ERROR",
     }
 
@@ -29,7 +30,11 @@ class Status:
         return tmp
 
     def set(self, bit_shift: int) -> None:
+        if bit_shift > self.STATUS_OVERRUN_ERROR:
+            return False
+
         self.value |= 1 << bit_shift
+        return True
 
     def to_value(self) -> int:
         return self.value
@@ -45,7 +50,11 @@ class Status:
 
         :return: A string showing the status register value and names set.
         """
-        set_names = [name for bit, name in Status._constant_map.items() if self.value & bit]
+        set_names = [
+            name
+            for bit_shift, name in Status.status_name_map.items()
+            if self.value & (1 << bit_shift)
+        ]
         set_names_str = ",".join(set_names) if set_names else "None"
         return f"Status(value=0x{self.value:02x}, names={set_names_str})"
 
