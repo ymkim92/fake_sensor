4782bc42a40bef30c33019f47c631f69c5aad586
Youngmin Kim <ymkim92@gmail.com>
2024-08-31 19:09:23 +1000

    WIP: adding communication

--- fake_sensor/interface/interface_rx_parser.py
+++ fake_sensor/interface/interface_rx_parser.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 from enum import Enum
+from typing import Optional
 
 
 class ParserState(Enum):
@@ -16,7 +17,10 @@ class ParserState(Enum):
 # pylint: disable=too-few-public-methods
 class IRxParser(ABC):
     @abstractmethod
-    def receive_message(self) -> bytes:
+    def receive_message(self, data: bytes) -> Optional[bytes]:
+        """returns message when it collects a valid message
+        input data can be a part of message
+        """
         pass
 
     @abstractmethod
