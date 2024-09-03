e7ed021d9784eda38e846757baa78cef655aac72
Youngmin Kim <ymkim92@gmail.com>
2024-09-03 19:10:54 +1000

    add cli

---
+++ cli.py
@@ -0,0 +1,22 @@
+"""fake sensor command line interface"""
+
+import time
+
+from fake_sensor.communication.zeromq_sub_pub import ZeroMqSubPub
+
+COMMUNICATION_TOPIC = b"sensor"
+COMMUNICATION_PORT = "tcp://127.0.0.1:5555"
+
+
+def main():
+    zero_mq = ZeroMqSubPub(COMMUNICATION_TOPIC, COMMUNICATION_PORT)
+    zero_mq.connect()
+    time.sleep(0.1)
+    zero_mq.send_data(b"0123")
+    time.sleep(0.1)
+    print(zero_mq.receive_data())
+    zero_mq.disconnect()
+
+
+if __name__ == "__main__":
+    main()
