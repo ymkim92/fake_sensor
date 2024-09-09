a29b2c125c27adbd7131b4ea2e82db0c294a8a14
Youngmin Kim <ymkim92@gmail.com>
2024-09-09 19:21:45 +1000

    working queue sub/pub

--- fake_sensor/communication/queue_sub_pub.py
+++ fake_sensor/communication/queue_sub_pub.py
@@ -1,13 +1,14 @@
 """queue"""
 
-from queue import Empty, Full, Queue
+from queue import Queue
+from typing import Any, Optional
 
 from .interface_communication import ICommunication
 
 
 class QueueSubPub(ICommunication):
-    def __init__(self, queue_size):
-        self.queue = Queue(maxsize=queue_size)
+    def __init__(self, queue_size: int):
+        self.queue: Queue[Any] = Queue(maxsize=queue_size)
         self.queue_size = queue_size
 
     def connect(self) -> bool:
@@ -17,7 +18,12 @@ class QueueSubPub(ICommunication):
         return True
 
     def send_data(self, data: bytes) -> bool:
+        if self.queue.full():
+            return False
         self.queue.put_nowait(data)
+        return True
 
-    def receive_data(self) -> bytes:
-        return self.queue.get_nowait()
+    def receive_data(self) -> Optional[bytes]:
+        if self.queue.empty():
+            return None
+        return bytes(self.queue.get_nowait())
--- tests/test_queue_sub_pub.py
+++ tests/test_queue_sub_pub.py
@@ -6,13 +6,12 @@ import pytest
 
 from fake_sensor.communication.queue_sub_pub import QueueSubPub
 
-SUB_QUEUE_SIZE = 2
-PUB_QUEUE_SIZE = 2
+QUEUE_SIZE = 2
 
 
 @pytest.fixture(name="queue_sub_pub_fixture")
 def queue_sub_pub() -> QueueSubPub:
-    return QueueSubPub(SUB_QUEUE_SIZE, PUB_QUEUE_SIZE)
+    return QueueSubPub(QUEUE_SIZE)
 
 
 def test_queue_sub_pub(queue_sub_pub_fixture: QueueSubPub) -> None:
@@ -21,9 +20,8 @@ def test_queue_sub_pub(queue_sub_pub_fixture: QueueSubPub) -> None:
 
     assert mq.send_data(b"0123")
     assert mq.send_data(b"012a")
-    # This will be dropped silently
-    assert mq.send_data(b"012b")
-    time.sleep(0.1)
+    assert mq.send_data(b"012b") is False
+
     assert mq.receive_data() == b"0123"
     assert mq.receive_data() == b"012a"
     assert mq.receive_data() is None
